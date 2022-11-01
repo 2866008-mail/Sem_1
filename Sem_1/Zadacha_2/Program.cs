aac9808bf62b9f6f987b9a24bb1425b7da7d21f3
Artyem M <2866008@mail.ru>
2022-11-01 05:58:46 +0300

    Записали 2 задачу

---
+++ Sem_1/Zadacha_2/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая на вход получает два целых числа и проверяет является ли первое число квадратом второго
+
+console.writeline("Введите число 1 = ");
+        int numberA = convert.toint32(console.readline());
+        console.writeline("Введите число 2 = ");
+        int numberB = convert.toint32(console.readline());
+
+        if (numberA == numberB * numberB)
+        {
+            console.writeline($"Правда: число А={numberA} является квадратом числа В = {numberB} ");
+        }
+        else
+        {
+            console.writeline($"Лож: число А={numberA} не является квадратом числа В = {numberB} ");
+        }
