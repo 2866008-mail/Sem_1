01c0971005b749b003de1459c127b8e6e5192996
Artyem M <2866008@mail.ru>
2022-11-01 14:47:18 +0300

    Исправил синтаксические ошибки во второй задачи

--- Sem_1/Zadacha_2/Program.cs
+++ Sem_1/Zadacha_2/Program.cs
@@ -1,15 +1,15 @@
 ﻿// Напишите программу, которая на вход получает два целых числа и проверяет является ли первое число квадратом второго
 
-console.writeline("Введите число 1 = ");
-        int numberA = convert.toint32(console.readline());
-        console.writeline("Введите число 2 = ");
-        int numberB = convert.toint32(console.readline());
+Console.Write("Введите число 1 = ");
+        int numberA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число 2 = ");
+        int numberB = Convert.ToInt32(Console.ReadLine());
 
         if (numberA == numberB * numberB)
         {
-            console.writeline($"Правда: число А={numberA} является квадратом числа В = {numberB} ");
+            Console.WriteLine($"Правда: число А={numberA} является квадратом числа В = {numberB} ");
         }
         else
         {
-            console.writeline($"Лож: число А={numberA} не является квадратом числа В = {numberB} ");
+            Console.WriteLine($"Лож: число А={numberA} не является квадратом числа В = {numberB} ");
         }
