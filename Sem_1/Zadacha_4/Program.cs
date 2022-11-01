fd755f9f174551df65cc36d07fb674ccf0caef2b
Artyem M <2866008@mail.ru>
2022-11-01 15:30:56 +0300

    Написали 4 задачю, проверили работоспособность(Работает)

---
+++ Sem_1/Zadacha_4/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Введите число N: ");
+int n=Convert.ToInt32(Console.ReadLine());
+int xn=n*-1;
+while(xn<=n)
+{
+Console.WriteLine(xn);
+xn=xn+1;
+}
+
