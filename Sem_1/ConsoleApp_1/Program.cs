c678b251eeb2028b69b9c3e3c613c68f375645c8
Artyem M <2866008@mail.ru>
2022-11-01 05:41:13 +0300

    Hello, World

---
+++ Sem_1/ConsoleApp_1/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Console.Write("Введите целое число  ");
+
+/*Int - переменная для целого числа, number - обозначение переменной,
+Convert.ToInt32(Console.ReadLine()); - данная команда принимает значение которое ввел пользователь 
+и преоброзует его (Convert.ToInt32) в целое число   */
+
+int number=Convert.ToInt32(Console.ReadLine());
+
+
+
