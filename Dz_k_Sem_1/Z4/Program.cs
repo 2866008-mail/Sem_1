ae934578e9ad3dc45e6b364d9bf9a435c98b797d
Artyem M <2866008@mail.ru>
2022-11-01 16:33:12 +0300

    Dz zadacha 4

---
+++ Dz_k_Sem_1/Z4/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+{
+ 
+        int a, b, c;
+Console.WriteLine("Введите 3 числа : ");
+ 
+        a = Convert.ToInt32(Console.ReadLine());
+ 
+        b = Convert.ToInt32(Console.ReadLine());
+
+        c = Convert.ToInt32(Console.ReadLine());
+ 
+         if (a > b && a > c)
+                    Console.WriteLine("Nubmer Max = " + a); 
+                else if (b > c)
+                    Console.WriteLine("Number Max = " + b); 
+                else
+                    Console.WriteLine("Number Max = " + c); 
+            
+            Console.ReadLine();
+ }
