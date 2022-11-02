1cdd9a182b5948cf5bec9b149c627bb81ed1c52f
Artyem M <2866008@mail.ru>
2022-11-02 13:09:49 +0300

    Dz zadacha 8 ura!!!!!!!!!!

--- Dz_k_Sem_1/Z8/Program.cs
+++ Dz_k_Sem_1/Z8/Program.cs
@@ -1,3 +1,10 @@
 ﻿/* Задача 8: Напишите программу, которая на вход принимает число (N), 
 а на выходе показывает все чётные числа от 1 до N.*/
+Console.WriteLine("Введите число");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Все четные числа в диапазоне от 1 до " + a);
+for (int i = 1; i <= a; i++)
+{
+    if (i % 2 == 0) {Console.WriteLine(i);}
+}
 
