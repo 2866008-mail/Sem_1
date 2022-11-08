25c97c6d5381e37fdf0f605411ecc43408c66b9c
Artyem M <2866008@mail.ru>
2022-11-08 12:22:27 +1100

    Sem_2 zadacha 1

--- Sem_2/Max_iz_3/Program.cs
+++ Sem_2/Max_iz_3/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Найти максимальное из 3 чисел
+
+Console.Write("Введите число A: ");
+int A=Convert.ToInt32(Consol.ReadLine());
+
+Console.Write("Введите число B: ");
+int B=Convert.ToInt32(Consol.ReadLine());
+
+Console.Write("Введите число C: ");
+int C=Convert.ToInt32(Consol.ReadLine());
+
+int max=A;
+if(B>max)
+{
+max=B
+}
+if(C>max)
+{
+    max=C
+}
+
+Console.WriteLine($"Максимальное из 3 чисел : {max}");
+
+Console.ReadKey();
